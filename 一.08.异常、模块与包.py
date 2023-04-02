5b988da32c160f17601b0364348fced83560dce7
etlpat <wsszw123321@163.com>
2023-04-02 21:01:17 +0800

    Py学习

--- 一.08.异常、模块与包.py
+++ 一.08.异常、模块与包.py
@@ -5,6 +5,7 @@
 3.异常的传递
 4.Python模块
 5.Python包
+6.综合案例
 """
 import my_module_test1
 import my_package_test1.my_module1
@@ -528,7 +529,7 @@ from my_package_test1 import my_module1
 
 
 ## （1）导入包中的模块
-# from 包名 import 模块名
+# from 包名 import 模块名1, 模块名2,...
 # 模块名.功能名()
 
 # from my_package_test1 import my_module1
@@ -627,5 +628,33 @@ from my_package_test1 import my_module1
 
 
 
+
+
+
+
+### 6.综合案例
+
+# 创建一个自定义包，名称为：my_utils（我的工具）
+# 在包内提供两个模块
+#   str_util.py（字符串相关工具，内含：）
+#       函数：str_reverse(s)，接收传入的字符串，将字符串反转返回
+#       函数：substr(s,x,y)，按照下标x和y，对字符串进行切片
+#   file_util.py（文件处理相关工具，内含：）
+#       函数：print_file_info(file_name)，接收传入文件的路径，打印文件的全部内容，
+#                                       如文件不存在则捕获异常，输出提示信息，最后关闭文件对象
+#       函数：append_to_file(file_name, data)，接收文件路径以及传入数据，将数据追加写入文件中
+
+
+# # 自定义包结果在该项目下的my_utils中
+#
+# from my_utils import file_util, str_util
+# s1 = str_util.str_reverse('1234567')
+# print(s1)  # 7654321
+# s2 = str_util.substr('1234567', 3, 6)
+# print(s2 + '\n')  # 456
+#
+# file_util.print_file_info('D:\Python\python项目\python_learn\测试文档\文本.txt')
+# file_util.append_to_file('D:\Python\python项目\python_learn\测试文档\文本.txt','asdfghj')
+# file_util.print_file_info('D:\Python\python项目\python_learn\测试文档\文本.txt')
 
 
